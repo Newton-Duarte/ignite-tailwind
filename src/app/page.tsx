482d3098634baf630a53bc256f845ff738628996
Newton Duarte <newton_@hotmail.com>
2023-09-16 06:09:27 -0300

    feat: add app layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <div className="h-screen bg-slate-900 p-4 text-slate-100">
-      <h1 className="font-bold">Hello Tailwind</h1>
-      <h2 className="text-3xl font-medium">Hello</h2>
-      <strong>World</strong>
-    </div>
-  )
+  return <h1 className="font-bold">Hello Tailwind</h1>
 }
