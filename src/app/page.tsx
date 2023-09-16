cda17e2a6aa8d728ca2f9598a6e4155a74d1cbad
Newton Duarte <newton_@hotmail.com>
2023-09-16 06:00:53 -0300

    chore: config eslint with prettier and fix on save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <div className="p-4 h-screen bg-slate-900 text-slate-100">
+    <div className="h-screen bg-slate-900 p-4 text-slate-100">
       <h1 className="font-bold">Hello Tailwind</h1>
-      <h2>Hello</h2>
+      <h2 className="text-3xl font-medium">Hello</h2>
       <strong>World</strong>
     </div>
   )
