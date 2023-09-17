da64999d808ac7593f0a8820b569775427d6571a
Newton Duarte <newton_@hotmail.com>
2023-09-17 11:03:25 -0300

    feat: add profile component

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,24 @@
+import { LogOut } from 'lucide-react'
+
+export function Profile() {
+  return (
+    <div className="grid grid-cols-profile items-center gap-3">
+      <img
+        src="https://github.com/newton-duarte.png"
+        alt="newton duarte github avatar"
+        className="h-10 w-10 rounded-full"
+      />
+      <div className="flex flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700">
+          Newton Duarte
+        </span>
+        <span className="trucante text-sm text-zinc-500">
+          newton_@hotmail.com
+        </span>
+      </div>
+      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  )
+}
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,18 +1,53 @@
-import { Search } from 'lucide-react'
+import {
+  BarChart,
+  CheckSquare,
+  Cog,
+  Flag,
+  Home,
+  LifeBuoy,
+  Search,
+  SquareStack,
+  Users,
+} from 'lucide-react'
 import { Logo } from './Logo'
+import { NavItem } from './NavItem'
+import { UsedSpaceWidget } from './UsedSpaceWidget'
+import { Profile } from './Profile'
 
 export function Sidebar() {
   return (
-    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <div className="item-center shadow-ms flex w-full gap-2 rounded-lg border border-zinc-300 px-3 py-2">
+      <div className="item-center shadow-ms mx-1 flex w-full gap-2 rounded-lg border border-zinc-300 px-3 py-2">
         <Search className="h-5 w-5 text-zinc-500" />
         <input
           type="text"
           placeholder="Search"
           className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
         />
+      </div>
+
+      <nav className="-space-y-0.5">
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
+      </nav>
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
+
+        <UsedSpaceWidget />
+
+        <div className="h-px bg-zinc-200"></div>
+
+        <Profile />
       </div>
     </aside>
   )
