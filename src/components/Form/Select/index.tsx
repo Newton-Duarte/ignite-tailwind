81c0f62c34d7ed917acd5c42b5bddce2f1a483eb
Newton Duarte <newton_@hotmail.com>
2023-09-19 06:34:41 -0300

    feat: add select component

---
+++ src/components/Form/Select/index.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import * as RadixSelect from '@radix-ui/react-select'
+import { ChevronDown } from 'lucide-react'
+import { ReactNode } from 'react'
+
+export type SelectProps = RadixSelect.SelectProps & {
+  children: ReactNode
+  placeholder: string
+}
+
+export function Select({ children, placeholder, ...props }: SelectProps) {
+  return (
+    <RadixSelect.Root {...props}>
+      <RadixSelect.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm data-[placeholder]:text-zinc-600">
+        <RadixSelect.Value placeholder={placeholder} className="text-black" />
+        <RadixSelect.Icon>
+          <ChevronDown className="h-5 w-5 text-zinc-500" />
+        </RadixSelect.Icon>
+      </RadixSelect.Trigger>
+
+      <RadixSelect.Portal>
+        <RadixSelect.Content
+          side="bottom"
+          position="popper"
+          sideOffset={8}
+          className="z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm"
+        >
+          <RadixSelect.Viewport className="outline-none">
+            {children}
+          </RadixSelect.Viewport>
+        </RadixSelect.Content>
+      </RadixSelect.Portal>
+    </RadixSelect.Root>
+  )
+}
