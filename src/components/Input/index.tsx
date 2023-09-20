5d4678bda4aae5e185eb984b85a74060374b7fd1
Newton Duarte <newton_@hotmail.com>
2023-09-20 05:19:32 -0300

    style: remove outline and add border when focused

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -11,7 +11,7 @@ type InputControlProps = ComponentProps<'input'>
 export function InputControl(props: InputControlProps) {
   return (
     <input
-      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
       {...props}
     />
   )
@@ -22,7 +22,7 @@ export type InputRootProps = ComponentProps<'div'>
 export function InputRoot(props: InputRootProps) {
   return (
     <div
-      className="item-center shadow-ms flex w-full gap-2 rounded-lg border border-zinc-300 px-3 py-2"
+      className="item-center shadow-ms flex w-full gap-2 rounded-lg border border-zinc-300 px-3 py-2 focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100"
       {...props}
     />
   )
