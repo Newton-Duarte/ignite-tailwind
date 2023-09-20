1976cecdb2beb4a9624a533f1f70515575c669dc
Newton Duarte <newton_@hotmail.com>
2023-09-20 05:20:27 -0300

    style: remove outline and add border when parent focused

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -13,9 +13,11 @@ export function TabItem({ title, value, isSelected = false }: TabItemProps) {
   return (
     <Tabs.Trigger
       value={value}
-      className="text-zinc-500.hover:text-violet-700 relative px-1 pb-4 text-sm font-medium hover:text-violet-700 data-[state=active]:text-violet-700"
+      className="text-zinc-500.hover:text-violet-700 group relative px-1 pb-4 text-sm font-medium outline-none hover:text-violet-700 data-[state=active]:text-violet-700"
     >
-      <span>{title}</span>
+      <span className="rounded group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">
+        {title}
+      </span>
       {isSelected && (
         <motion.div
           layoutId="activeTab"
